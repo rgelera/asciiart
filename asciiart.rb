c4c4b16830980148fc74f474cda06869c1afb4a4
Rodney Gelera <rodney.gelera@shopify.com>
2021-05-28 09:35:23 -0700

    part 2 completed

--- asciiart.rb
+++ asciiart.rb
@@ -1,9 +1,9 @@
 # ASCII Art Ruby Assignment
 
 print "Enter a number: "
-# assumption is always an integer input
+# assumption is always an integer input.
 num = gets.to_i
 
 for i in 1..num
-  puts "*" * i
+  puts "#{" " * (num - i)}#{"*" * i}"
 end
